e707332fc9e64b842630f7b6458bf720ab16d89b
Francois Blanchette <cfrankb.2000@gmail.com>
2022-12-28 23:03:27 -0500

    fixed linux release

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,17 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
-    if (!translator.load(":/translations/translation_fr")) {
+    /*QTranslator translator;
+    if (!translator.load(":/translations/translation_fr"))
+    {
         printf("cannot find translation\n");
     }
-    a.installTranslator(&translator);
+    a.installTranslator(&translator);*/
 
     MainWindow w;
     w.setWindowIcon(QIcon(":/images/icon.gif"));
-    if (argc > 1) {
+    if (argc > 1)
+    {
         w.setDocument(argv[1]);
     }
 
